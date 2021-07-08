85714d585ce640d1f900900b454017fc5faa01a8
Alec <alecech1994@gmail.com>
2021-07-08 12:37:44 -0700

    Added event handlers and useEffect hooks to query the server and update state of user

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 // import Home from './Home';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import Register from './Register';
 import AuthContext from '../contexts/AuthContext';
+import UserContext from '../contexts/UserContext';
 import '../styles.sass';
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  return (
+  const [username, setUsername] = useState(null);
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:6969/dashboard',
+    })
+      .then((res) => {
+        if (res.data !== 'Invalid User Credentials') {
+          setUsername(res.data);
+          setLoggedIn(true);
+        }
+      })
+      .catch(console.error)
+      .finally(() => {
+        setRendered(true);
+      });
+  });
+  return rendered ? (
     <div>
       <Switch>
         <AuthContext.Provider value={[loggedIn, setLoggedIn]}>
-          <Route exact path="/">
-            {loggedIn ? <Redirect to="/dashboard" /> : <Redirect to="/login" /> }
-          </Route>
-          <Route exact path="/dashboard">
-            {loggedIn ? <Dashboard /> : <Redirect to="/login" />}
-          </Route>
-          <Route exact path="/login">
-            {loggedIn ? <Redirect to="/dashboard" /> : <Login />}
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
+          <UserContext.Provider value={[username, setUsername]}>
+            <Route exact path="/">
+              {loggedIn ? <Redirect to="/dashboard" /> : <Redirect to="/login" /> }
+            </Route>
+            <Route exact path="/dashboard">
+              {loggedIn ? <Dashboard user={username} /> : <Redirect to="/login" />}
+            </Route>
+            <Route exact path="/login">
+              {loggedIn ? <Redirect to="/dashboard" /> : <Login />}
+            </Route>
+            <Route exact path="/register">
+              {loggedIn ? <Redirect to="/dashboard" /> : <Register />}
+            </Route>
+          </UserContext.Provider>
         </AuthContext.Provider>
       </Switch>
     </div>
-  );
+  ) : null;
 };
 
 export default App;
