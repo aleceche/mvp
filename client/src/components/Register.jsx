bb763cedaafbf89f35c0e9fb6cbb574cc425a13f
Alec <alecech1994@gmail.com>
2021-07-08 12:36:33 -0700

    Updated register page styling and the ability to go back to the login page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Register = () => {
@@ -7,7 +8,8 @@ const Register = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const login = () => {
+  const login = (e) => {
+    e.preventDefault();
     axios({
       method: 'post',
       data: {
@@ -20,33 +22,36 @@ const Register = () => {
       withCredentials: true,
       url: 'http://localhost:6969/register',
     })
-      .then((res) => console.log(res.data));
+      .then(() => {
+        // e.target.reset();
+      });
   };
   return (
     <div id="register">
-      <form>
+      <form onSubmit={login}>
         <div className="header">
           Create An Account
         </div>
         <div className="credentials">
           <label htmlFor="username">
-            <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
           </label>
           <label htmlFor="password">
-            <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
           </label>
           <label htmlFor="firstName">
-            <input type="text" name="firstName" placeholder="Bob" onChange={(e) => setFirstName(e.target.value)} />
+            <input type="text" name="firstName" placeholder="First Name (e.g. Bob)" onChange={(e) => setFirstName(e.target.value)} required />
           </label>
           <label htmlFor="lastName">
-            <input type="text" name="lastName" placeholder="Jones" onChange={(e) => setLastName(e.target.value)} />
+            <input type="text" name="lastName" placeholder="Last Name (e.g. Jones)" onChange={(e) => setLastName(e.target.value)} required />
           </label>
           <label htmlFor="email">
-            <input type="email" name="email" placeholder="bobjones@gmail.com" onChange={(e) => setEmail(e.target.value)} />
+            <input type="email" name="email" placeholder="Email (e.g. bobjones@gmail.com)" onChange={(e) => setEmail(e.target.value)} required />
           </label>
         </div>
         <div className="btns">
-          <button type="button" onClick={login} className="btn">Create Account</button>
+          <Link to="/login" className="link">Back to Login</Link>
+          <button type="submit" className="btn">Create Account</button>
         </div>
       </form>
     </div>
